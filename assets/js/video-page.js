258540cb70d5764a9567be8cc4f6195f0bb1e734
Dylan Graham <dylan.graham@gmail.com>
2017-02-26 22:37:23 +1100

    3.6.1

--- assets/js/video-page.js
+++ assets/js/video-page.js
@@ -3,7 +3,7 @@ $(function whenDomIsReady() {
   $('.the-list-of-videos .loading').show();
 
   setTimeout(function afterRetrievingVideos() {
-    const videos = [{
+    var videos = [{
       title: 'FUNNY BABY VIDEOS',
       src: 'https://www.youtube.com/embed/_FvTVWjLiHM'
     }, {
@@ -14,14 +14,51 @@ $(function whenDomIsReady() {
       src: 'https://www.youtube.com/embed/_OBlgSz8sSM'
     }];
 
-    $(`.the-list-of-videos .loading`).hide();
+    $('.the-list-of-videos .loading').hide();
 
-    const videosHtml = _.reduce(videos, (html, video) => {
-      html += `<li class="video">  <h2>${video.title}</h2>  <iframe width="640" height="390" src="${video.src}" frameborder="0" allowfullscreen></iframe></li>`;
+    var videosHtml = _.reduce(videos, function (html, video) {
+      html += '<li class="video">' +
+        '  <h2>' + video.title + '</h2>' +
+        '  <iframe width="640" height="390" src="' + video.src + '" frameborder="0" allowfullscreen></iframe>' +
+        '</li>';
       return html;
     }, '');
 
-    $(`.the-list-of-videos ul`).replaceWith(videosHtml);
+    $('.the-list-of-videos ul').replaceWith(videosHtml);
 
   }, 750);
+
+  $('.submit-video-form').submit(function (e) {
+
+    e.preventDefault();
+
+    var newVideo = {
+      title: $('.submit-video-form input[name="title"]').val(),
+      src: $('.submit-video-form input[name="src"]').val()
+    };
+
+    $('.submit-video-form input').val('');
+    $('.submit-video-form button').text('Submitting...');
+    $('.submit-video-form button').prop('disabled', true);
+
+    var parser = document.createElement('a');
+
+    parser.href = newVideo.src
+
+    var youtubeID = parser.search.substring(parser.search.indexOf("=") + 1, parser.search.length);
+
+    newVideo.src = 'https://www.youtube.com/embed/' + youtubeID;
+
+    setTimeout(function () {
+      var newVideoHtml = '<li class="video">' +  //#E
+        '  <h2>' + newVideo.title + '</h2>' +
+        '  <iframe width="640" height="390" src="' + newVideo.src + '" frameborder="0" allowfullscreen></iframe>' +
+        '</li>';
+
+      $('.the-list-of-videos').prepend(newVideoHtml); //#F
+      $('.submit-video-form button').text('Submit Video');
+      $('.submit-video-form button').prop('disabled', false);
+
+    }, 750);
+  });
 });
